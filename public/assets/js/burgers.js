4b004b6fd92e7ae43a1686764440988dc6354064
frankcodes <alanforducf@gmail.com>
2020-03-25 12:45:44 -0400

    still no response from clicks

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,36 +1,43 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newDevour = $(this).data("newdevour");
-  
-    var newDevourState = {
-      devoured: newDevour
+    var newdevour = $(this).data("newdevour");
+
+    var newdevourState = {
+      devoured: newdevour
     };
-  
+
+    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newDevourState
+      data: newdevourState
     }).then(
       function() {
-        console.log("changed devour to", newDevour);
+        console.log("changed devour to", newdevour);
+        // Reload the page to get the updated list
         location.reload();
       }
     );
-  });  
+  });
+
   $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
     event.preventDefault();
-  
+
     var newBurger = {
       burger_name: $("#ba").val().trim(),
       devoured: false
     };
-  
+
+    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
+        // Reload the page to get the updated list
         location.reload();
       }
     );
