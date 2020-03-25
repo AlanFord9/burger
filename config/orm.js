aa08667cdd5eb546a79066f853598f1a34b3c075
frankcodes <alanforducf@gmail.com>
2020-03-25 15:09:40 -0400

    moved js into main, now fully functioning

--- config/orm.js
+++ config/orm.js
@@ -1,78 +1,32 @@
 var connection = require("../config/connection.js");
 
-function printQuestionMarks(num) {
-  var arr = [];
-
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
-function objToSql(ob) {
-  var arr = [];
-
-  for (var key in ob) {
-    var value = ob[key];
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      arr.push(key + "=" + value);
-    }
-  }
-
-  return arr.toString();
-}
-
 var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
+    selectAll: function (callback) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, res) {
+            if (err) throw err;
+            console.log(res);
+            callback(res)
+        });
+    },
+    insertOne: function (burger, callback) {
+        var queryString = "INSERT INTO burgers (burger_name, devoured) values ('" + burger + "', '0')";
+        console.log(queryString);
+        connection.query(queryString, function (err, res) {
+            if (err) throw err;
+            console.log(res);
+            callback(res);
+        });
+    },
+    updateOne: function (id, callback) {
+        var queryString = "UPDATE burgers SET devoured = 1 WHERE id = " + id;
+        console.log(queryString);
+        connection.query(queryString, function (err, res) {
+            if (err) throw err;
+            console.log(res);
+            callback(res);
+        });
+    }
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,21 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+    all: function (callback) {
+        orm.selectAll(function (res) {
+            callback(res);
+        });
+    },
+    create: function (burger, callback) {
+        orm.insertOne(burger, function (res) {
+            callback(res);
+        });
+    },
+    update: function (id, condition, callback) {
+        orm.updateOne(id, condition, function (res) {
+            callback(res);
+        });
+    }
 };
 
-// Export the database functions for the controller (burgersController.js).
 module.exports = burger;
